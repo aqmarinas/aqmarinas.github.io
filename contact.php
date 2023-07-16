8feda2e093a4d35227f5832b9ec2528b48293464
Aqmarina Shabihah <aqmarinasha@gmail.com>
2023-07-17 00:56:07 +0700

    initial commit

---
+++ contact.php
@@ -0,0 +1,35 @@
+<?php
+// Konfigurasi database
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'porto';
+
+// Menghubungkan ke database
+$koneksi = mysqli_connect($host, $username, $password, $database);
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+// Memeriksa apakah form telah dikirim
+if (isset($_POST['submit'])) {
+    // Mengambil nilai input dari form
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // Menyimpan data ke database
+    $sql = "INSERT INTO contact (name, email, message) VALUES ('$name', '$email', '$message')";
+
+    if (mysqli_query($koneksi, $sql)) {
+        echo "<script>alert('Success');</script>";
+        
+    } else {
+        echo "<script>alert('Something went wrong');</script>";
+    }
+}
+echo "<script>window.location.href='index.html';</script>";
+
+// Menutup koneksi database
+mysqli_close($koneksi);
+?>
